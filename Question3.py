92676105d7f74448f3696b4d0d00edc9cdde06fe
Kalpnapandey <kalpnapandey43@gmail.com>
2025-06-05 15:44:04 +0530

    Add files via upload
    Python questions of variables

---
+++ Question3.py
@@ -0,0 +1,33 @@
+# Simple Assignment
+# Assign your name to a variable called name and print:
+# "Hello, <your_name>"
+name="Kalpna Pandey"
+print(f"Hello, {name}")
+
+# Storing Age
+# Create a variable age and assign your age to it. Then print:
+# "You are <age> years old"
+age=22
+print(f"You are {age} years old")
+
+# Swapping Variables
+# Assign any two numbers to variables a and b. Swap their values
+# using a third variable and print the results.
+a=2
+b=3
+c=a
+a=b
+b=c
+print(a,b)
+
+# Dynamic Assignment from Input
+# Ask the user to enter their city name and store it in a variable city. Then print:
+# "You live in <city>"
+city=input("Enter your city")
+print(f"You live in {city}")
+
+# Math with Variables
+# Assign two variables length = 10 and width = 5, then calculate the area of a rectangle and print it.
+length=10
+width=5
+print(f"Area of rectangle = {length*width}")
