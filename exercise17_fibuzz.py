4c45f916a152e267941ff78ebea797e2f377fbe0
Kalpnapandey <kalpnapandey43@gmail.com>
2025-06-05 10:52:02 +0530

    Add files via upload
    A conditional statement question

---
+++ exercise17_fibuzz.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number to end the range"))
+for i in range(0,n):
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    elif i%5==0:
+        print("Buzz")
+    elif i%3==0:
+        print("Fizz")
+    else:
+        print(i)
