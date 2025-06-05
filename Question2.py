61891631a0b168ace769505f990a581600857295
Kalpnapandey <kalpnapandey43@gmail.com>
2025-06-05 15:39:49 +0530

    Add files via upload
    Python questions using input() function

---
+++ Question2.py
@@ -0,0 +1,21 @@
+# Ask the user to enter their name and greet them.
+name=input("Enter your name")
+print("Hello ",name )
+
+# Ask the user to enter their age and print "You are X years old".
+age=input("Enter your age")
+print(f"You are {age} years old")
+
+# Ask for two numbers from the user and print their sum.
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+print(a+b)
+
+# Take a city name input from the user and print a sentence like: “City X is beautiful.”
+city=input("Enter your city")
+print(f"City {city} is beautiful")
+
+# Take name and age from user and print this: “My name is [name] and I am [age] years old.”
+n=input("Enter your name")
+a=input("Enter your age")
+print(f"My name is {n} and I am {a} years old")
