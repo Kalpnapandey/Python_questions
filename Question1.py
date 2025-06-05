0b1aa665b730f6eea735b49ed0e19960faa4f33b
Kalpnapandey <kalpnapandey43@gmail.com>
2025-06-05 15:38:48 +0530

    Add files via upload
    Basic python questions related to print() function

---
+++ Question1.py
@@ -0,0 +1,17 @@
+#Print your name and age using string concatenation.
+
+name=input("Enter your name")
+age=input("Enter your age")
+print(name + age)
+
+#Combine two strings "Hello" and "World" with a space in between and print the result.
+
+print("Hello"+" "+"World")
+
+#Print a sentence using variables: name = "Kalpna" and language = "Python".
+name=input("Enter your name")
+lang=input("Enter the language")
+print("Name=" ,name,"\n","Language=",lang)
+
+#Concatenate a string with a number (e.g., "I am " + 28). Fix any error that occurs.
+print("I am " +  str(28))
